40c6415986ca3b357ebba706c725fbfa9dfe15f1
Gasa Mzolo <gmzolo@dearx.co.za>
2021-08-19 13:17:02 +0200

    initial commit

---
+++ src/main/java/com/example/meneer/meneerdemo/MeneerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.meneer.meneerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeneerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeneerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meneer/meneerdemo/MeneerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meneer.meneerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeneerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
