cf1542afbd1488afe89459cf7e60b3cb9cc8a2de
Gasa Mzolo <gasam@grobank.co.za>
2021-08-19 13:34:21 +0200

    added controller

---
+++ src/main/java/com/example/meneer/meneerdemo/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.meneer.meneerdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String helloWorld() {
+		return "Hello Meneer";
+	}
+}
